0ebce0f020ef32c85b7b1a86574123b586f48653
Matt Wynne <matt@mattwynne.net>
2011-09-20 11:33:37 +0200

    Change to use let block, which is more idiomatic RSpec

--- 220/ruby/tictactoe/tictactoe_spec.rb
+++ 220/ruby/tictactoe/tictactoe_spec.rb
@@ -1,58 +1,56 @@
 require './tictactoe'
 
 describe TicTacToe do
-  
-  before(:each) do
-    @game = TicTacToe.new(3)
-  end
+
+  let(:game) { TicTacToe.new(3) }
   
   it "starts with an empty board" do
-    @game.show_board.should == "___" + "___" + "___"
+    game.show_board.should == "___" + "___" + "___"
   end
   
   it "allows players to place symbols using coordinates" do
-    @game.play_at(0,0, "X")
-    @game.show_board.should == "X__" + "___" + "___"
-    @game.play_at(1,1, "O")
-    @game.show_board.should == "X__" + "_O_" + "___"
+    game.play_at(0,0, "X")
+    game.show_board.should == "X__" + "___" + "___"
+    game.play_at(1,1, "O")
+    game.show_board.should == "X__" + "_O_" + "___"
   end
 
   it "prevents player playing in already filled place" do
-    @game.play_at(0,0, "X")
-    @game.play_at(0,0, "O")
-    @game.show_board.should == "X__" + "___" + "___"
+    game.play_at(0,0, "X")
+    game.play_at(0,0, "O")
+    game.show_board.should == "X__" + "___" + "___"
   end
   
   it "detects a horizontal winning line" do
-    @game.play_at(0,0,"X")
-    @game.play_at(0,1,"X")
-    @game.has_been_won.should == false
-    @game.play_at(0,2,"X")
-    @game.has_been_won.should == true
+    game.play_at(0,0,"X")
+    game.play_at(0,1,"X")
+    game.has_been_won.should == false
+    game.play_at(0,2,"X")
+    game.has_been_won.should == true
   end
 
   it "detects a vertical winning line" do
-    @game.play_at(0,0,"O")
-    @game.play_at(1,0,"O")
-    @game.has_been_won.should == false
-    @game.play_at(2,0,"O")
-    @game.has_been_won.should == true
+    game.play_at(0,0,"O")
+    game.play_at(1,0,"O")
+    game.has_been_won.should == false
+    game.play_at(2,0,"O")
+    game.has_been_won.should == true
   end
 
   it "detects a forward diagonal winning line" do
-    @game.play_at(0,0,"O")
-    @game.play_at(1,1,"O")
-    @game.has_been_won.should == false
-    @game.play_at(2,2,"O")
-    @game.has_been_won.should == true
+    game.play_at(0,0,"O")
+    game.play_at(1,1,"O")
+    game.has_been_won.should == false
+    game.play_at(2,2,"O")
+    game.has_been_won.should == true
   end
 
   it "detects a reverse diagonal winning line" do
-    @game.play_at(0,2,"O")
-    @game.play_at(1,1,"O")
-    @game.has_been_won.should == false
-    @game.play_at(2,0,"O")
-    @game.has_been_won.should == true
+    game.play_at(0,2,"O")
+    game.play_at(1,1,"O")
+    game.has_been_won.should == false
+    game.play_at(2,0,"O")
+    game.has_been_won.should == true
   end
   
   it "allows the board size to be specified and checks rules appropriately" do
